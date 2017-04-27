e624e8dc5229f589f370780219a86a203cd1fad3
Wojciech Hołowiak <w.holowiak@gmail.com>
2017-04-27 19:17:25 +0200

    MainActivity now is aware of user switching between BottomBar elements.

--- app/src/main/java/com/astronauts/astronaut/MainActivity.java
+++ app/src/main/java/com/astronauts/astronaut/MainActivity.java
@@ -1,13 +1,52 @@
 package com.astronauts.astronaut;
 
+import android.support.annotation.IdRes;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.FrameLayout;
+
+import com.roughike.bottombar.BottomBar;
+import com.roughike.bottombar.OnTabSelectListener;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
 
 public class MainActivity extends AppCompatActivity {
+    @BindView(R.id.contentContainer)
+    FrameLayout contentContainer;
+    @BindView(R.id.bottomBar)
+    BottomBar bottomBar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+
+        //Prevent fragments from accidental overlapping
+        if (savedInstanceState != null) {
+            return;
+        }
+
+        bottomBar.setOnTabSelectListener(new OnTabSelectListener() {
+            @Override
+            public void onTabSelected(@IdRes int tabId) {
+                if(tabId == R.id.tab_mirkoblog){
+
+                }
+                if(tabId == R.id.tab_moj_wykop){
+
+                }
+                if(tabId == R.id.tab_profile){
+
+                }
+                if(tabId == R.id.tab_settings){
+
+                }
+            }
+        });
+
     }
 }
+
