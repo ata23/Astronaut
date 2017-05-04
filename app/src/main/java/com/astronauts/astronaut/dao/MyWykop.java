c333bcecb0fb80228d65e870c1312404004e2681
Wojciech Hołowiak <w.holowiak@gmail.com>
2017-05-04 19:08:45 +0200

    MyWykop now works with entry type data.

--- app/src/main/java/com/astronauts/astronaut/dao/MyWykop.java
+++ app/src/main/java/com/astronauts/astronaut/dao/MyWykop.java
@@ -5,4 +5,19 @@ package com.astronauts.astronaut.dao;
  */
 
 public class MyWykop {
+
+    //Only entry functionality is implemented as we are more focused on mikroblog than main page right now
+    //For time being just filter JSON object so it only provides entry type objects
+    //NEEDS TESTING - Api documentation is too poorly written to understand what exactly it will work with
+
+    public enum type {link,entry}
+
+    private type dataType;
+    private EntryBasics[] entries;
+
+    public MyWykop(EntryBasics[] entries){
+        dataType=type.entry;
+        this.entries=entries;
+    }
+
 }
